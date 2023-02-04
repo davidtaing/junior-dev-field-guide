e8955dc458cf367713f947ad7d2c5afb6b15cb70
David Taing <adavidtaing@gmail.com>
2023-02-04 21:56:24 +1100

    build: add eslint
    - adds linting for TypeScript and Astro components.
    - add lint npm script
    
    fixes: #3

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  ignorePatterns: [".eslintrc.cjs", "tailwind.config.cjs", "node_modules/"],
+  extends: "eslint:recommended",
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    extraFileExtensions: [".astro"], // This is a required setting in `@typescript-eslint/parser` v5.
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    }
+  ]
+};
